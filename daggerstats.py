3ef5594ceb8ad4a2f1cb06522955ca319614317e
Radet 5 <rad.april.rachel@gmail.com>
2019-02-21 04:33:45 -0500

    This is in a kinda weird in-between state, but I added Social information to the faction data and started trying to parse the character record but something is wrong with either how I'm parsing the data or info I have on said data

--- daggerstats.py
+++ daggerstats.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python3
 import tabulate
+import os
+from io import BytesIO
 
-with open("SAVE1/SAVEVARS.DAT", "rb") as savevars:
+list_count = 7
+#path_prefix="/usr/share/games/daggerfall/DAGGER/"
+path_prefix=""
+latest_save_id = 0
+for i in range(0,6):
+    path = path_prefix+"SAVE"+str(i)+"/SAVEVARS.DAT"
+    mod_time = os.path.getmtime(path)
+    if mod_time > latest_save_id: new_latest_id = i
+    else: new_latest_id = latest_save_id
+    latest_save_id = new_latest_id
+#   print(mod_time)
+#print(latest_save_id)
+
+save_vars_path = path_prefix+"SAVE"+str(latest_save_id)+"/SAVEVARS.DAT"
+with open(save_vars_path, "rb") as savevars:
     initial_offset = 6096
     record_size = 92
     savevars.seek(initial_offset,0)
-    requests = [("Faction Name",3,26,"c"),\
-                ("Character Reputation", 29, 1,"s")]
+    requests = [("Name",3,26,"c"),\
+                ("Power",31,1,"s"),\
+                ("Social",54,1,"b"),\
+                ("Reputation", 29, 1,"s")]
     results = []
     for i in range(0, 366):
         pos = 0
@@ -36,9 +54,24 @@ with open("SAVE1/SAVEVARS.DAT", "rb") as savevars:
 #       print("\n")
         seek_value = record_size - pos
         savevars.seek(seek_value,1)
-    sorted_results = sorted(results, key=lambda k: k['Character Reputation'])
-    haters = sorted_results[:15]
-    lovers = sorted_results[-15:]
+    integers = []
+    for i in range(0,8):
+        integers.append(i.to_bytes(1,byteorder='little',signed=True))
+    social_lookup = {integers[0]:"Commoner",
+                     integers[1]:"Merchant",
+                     integers[2]:"Scholar",
+                     integers[3]:"Nobility",
+                     integers[4]:"Underworld",
+                     integers[6]:"Supernatural",
+                     integers[7]:"Kni/Temp/Vamp"}
+    for result in results:
+        try: 
+            social = social_lookup[result['Social']]
+        except KeyError: social = "Undefined"
+        result['Social'] = social
+    sorted_results = sorted(results, key=lambda k: k['Reputation'])
+    haters = sorted_results[:list_count]
+    lovers = sorted_results[-list_count:]
     lovers.reverse()
 
     hater_table = "=============\n    HATERS\n"+tabulate.tabulate(haters,headers="keys",tablefmt="rst")+"\n"
@@ -47,3 +80,34 @@ with open("SAVE1/SAVEVARS.DAT", "rb") as savevars:
     with open("haters.txt", "w") as txt_file: txt_file.write(hater_table)
     with open("lovers.txt", "w") as txt_file: txt_file.write(lover_table)
 
+    sorted_results = sorted(results, key=lambda k: k['Power'], reverse=True)
+    faction_table = "=============\n FACTIONS \n"+tabulate.tabulate(sorted_results,headers="keys",tablefmt="rst")+"\n"
+    with open("factions.txt", "w") as txt_file: txt_file.write(faction_table)
+
+save_tree_path = path_prefix+"SAVE"+str(latest_save_id)+"/SAVETREE.DAT"
+with open(save_tree_path, "rb") as savetree:
+    initial_offset = 19
+    cur_record_size = 0
+
+    savetree.seek(initial_offset,0)
+    for i in range(0,442):
+        cur_record_size = int.from_bytes(savetree.read(4),byteorder='little')
+        if cur_record_size > 0:
+            record_type_id = savetree.read(1)
+            print("Record "+str(i)+": <"+str(cur_record_size)+"> "+record_type_id.hex())
+            if record_type_id == int(3).to_bytes(1,byteorder='little',signed=True):
+                print("CHARACTER_RECORD!!")
+                character_record = BytesIO(savetree.read(cur_record_size))
+                seek_offset = cur_record_size + 1
+            else:
+                seek_offset = cur_record_size + 1
+                print(savetree.read(cur_record_size))
+            seek = cur_record_size - seek_offset
+            savetree.seek(seek,1)
+        else: 
+            print("Record "+str(i)+": <0>")
+            pass
+    character_record.seek(658)
+    print(character_record.read(24))
+    print(int.from_bytes(character_record.read(2),byteorder='little',signed=True))
+    print(character_record.read(32))
